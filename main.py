55b584319f8512057da1c5c277d058f17eb71930
ivanmonahov75 <ivanmonahov75@gmail.com>
2023-09-22 22:28:03 +0300

    small fixes

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import socket
 import sys
 import time
 from multiprocessing import Process
-
 import cv2
 
 
@@ -13,10 +12,11 @@ def com_stm():  # will be used for sending and receiving data from stm
 
 def com_client(q_stm, q_img):  # will be used for two-way communication with client (desktop)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('127.0.0.1', 9090))
+    server_socket.bind(('127.0.0.1', 8080))
     server_socket.listen(1)
     while True:
         client_socket, adr = server_socket.accept()
+        print(f'connected: {adr}')
         state = True
         while state:
             data_got = client_socket.recv(60)
@@ -46,7 +46,7 @@ def capturing(q):  # just for separate video capture
         encoded_image = raw_frame.tobytes()
         q.empty()
         q.put(encoded_image)  # transfer image to queue, so other tasks can access it
-        time.sleep(0.06)
+        time.sleep(0.03)
 
 
 if __name__ == '__main__':
