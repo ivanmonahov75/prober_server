b7615152e5070e18834794fe941baf348e35c0b8
ivanm <ivanm@pop-os.localdomain>
2023-10-14 18:17:58 +0300

    stm fix

--- stm_class.py
+++ stm_class.py
@@ -1,24 +1,24 @@
 import serial
 
 
-class STM_comm(serial.Serial):
+class STM_comm():
     def __init__(self, speed, **kwargs):
         super().__init__(**kwargs)
         for i in range(3):
             try:
-                self.Serial(f'/dev/ttyACM{0}', 12000000)
+                self.ser = serial.Serial(f'/dev/ttyACM{i}', 12000000)
                 self.opened = True
                 return
             except:
                 print(f'No /dev/ttyACM{i}')
 
     def comm(self, adr, *args):
-        if self.Serial:
+        if self.ser:
             # make bytes array for sending
             mess = adr + '\n'
-            self.write(mess)
+            self.ser.write(mess)
             # read what we got
-            ret = self.readline()
+            ret = self.ser.readline()
             # modify it to dict
             return ret
 
